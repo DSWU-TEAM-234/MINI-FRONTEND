6e0ea975e492094746d1e94447b20a9f4aa5e135
SAY-Hong <bobseahee@gmail.com>
2024-10-21 02:04:25 +0900

    ADD: 아바타 커스텀 파일 추가

---
+++ src/components/AllclothingOption.js
@@ -0,0 +1,160 @@
+// 상의 - 색상별
+export const topOptions = [
+  { id: 1, name: "반팔_검정", image: "/clothes/top/Tshirt/Tshirt_black.png" },
+  { id: 2, name: "반팔_하늘", image: "/clothes/top/Tshirt/Tshirt_blue.png" },
+  { id: 3, name: "반팔_초록", image: "/clothes/top/Tshirt/Tshirt_green.png" },
+  { id: 4, name: "반팔_인디고", image: "/clothes/top/Tshirt/Tshirt_indigo.png" },
+  { id: 5, name: "반팔_갈색", image: "/clothes/top/Tshirt/Tshirt_orange.png" },
+  { id: 6, name: "반팔_분홍", image: "/clothes/top/Tshirt/Tshirt_purple.png" },
+  { id: 7, name: "반팔_빨강", image: "/clothes/top/Tshirt/Tshirt_red.png" },
+  { id: 8, name: "반팔_노랑", image: "/clothes/top/Tshirt/Tshirt_yellow.png" },
+  { id: 9, name: "반팔_흰색", image: "/clothes/top/Tshirt/Tshirt.png" },
+  
+  { id: 10, name: "긴팔_검정", image: "/clothes/top/Tshirt_Long/Tshirt_long_black.png" },
+  { id: 11, name: "긴팔_하늘", image: "/clothes/top/Tshirt_Long/Tshirt_long_blue.png" },
+  { id: 12, name: "긴팔_초록", image: "/clothes/top/Tshirt_Long/Tshirt_long_green.png" },
+  { id: 13, name: "긴팔_인디고", image: "/clothes/top/Tshirt_Long/Tshirt_long_indigo.png" },
+  { id: 14, name: "긴팔_갈색", image: "/clothes/top/Tshirt_Long/Tshirt_long_orange.png" },
+  { id: 15, name: "긴팔_분홍", image: "/clothes/top/Tshirt_Long/Tshirt_long_purple.png" },
+  { id: 16, name: "긴팔_빨강", image: "/clothes/top/Tshirt_Long/Tshirt_long_red.png" },
+  { id: 17, name: "긴팔_노랑", image: "/clothes/top/Tshirt_Long/Tshirt_long_yellow.png" },
+  { id: 18, name: "긴팔_흰색", image: "/clothes/top/Tshirt_Long/Tshirt_long.png" },
+
+  { id: 19, name: "후드티_검정", image: "/clothes/top/hood/hood_black.png" },
+  { id: 20, name: "후드티_하늘", image: "/clothes/top/hood/hood_blue.png" },
+  { id: 21, name: "후드티_초록", image: "/clothes/top/hood/hood_green.png" },
+  { id: 22, name: "후드티_인디고", image: "/clothes/top/hood/hood_indigo.png" },
+  { id: 23, name: "후드티_갈색", image: "/clothes/top/hood/hood_orange.png" },
+  { id: 24, name: "후드티_분홍", image: "/clothes/top/hood/hood_purple.png" },
+  { id: 25, name: "후드티_빨강", image: "/clothes/top/hood/hood_red.png" },
+  { id: 26, name: "후드티_노랑", image: "/clothes/top/hood/hood_yellow.png" },
+  { id: 27, name: "후드티_흰색", image: "/clothes/top/hood/hood.png" },
+
+  { id: 28, name: "셔츠_검정", image: "/clothes/top/shirt/shirt_black.png" },
+  { id: 29, name: "셔츠_하늘", image: "/clothes/top/shirt/shirt_blue.png" },
+  { id: 30, name: "셔츠_초록", image: "/clothes/top/shirt/shirt_green.png" },
+  { id: 31, name: "셔츠_인디고", image: "/clothes/top/shirt/shirt_indigo.png" },
+  { id: 32, name: "셔츠_갈색", image: "/clothes/top/shirt/shirt_orange.png" },
+  { id: 33, name: "셔츠_분홍", image: "/clothes/top/shirt/shirt_purple.png" },
+  { id: 34, name: "셔츠_빨강", image: "/clothes/top/shirt/shirt_red.png" },
+  { id: 35, name: "셔츠_노랑", image: "/clothes/top/shirt/shirt_yellow.png" },
+  { id: 36, name: "셔츠_흰색", image: "/clothes/top/shirt/shirt.png" },
+];
+
+
+// 하의 - 색상별
+export const bottomOptions = [
+  // 반바지
+  { id: 1, name: "반바지_검정", image: "/clothes/bottom/pants2/pants2_black.png" },
+  { id: 2, name: "반바지_하늘", image: "/clothes/bottom/pants2/pants2_blue.png" },
+  { id: 3, name: "반바지_초록", image: "/clothes/bottom/pants2/pants2_green.png" },
+  { id: 4, name: "반바지_인디고", image: "/clothes/bottom/pants2/pants2_indigo.png" },
+  { id: 5, name: "반바지_갈색", image: "/clothes/bottom/pants2/pants2_orange.png" },
+  { id: 6, name: "반바지_분홍", image: "/clothes/bottom/pants2/pants2_purple.png" },
+  { id: 7, name: "반바지_빨강", image: "/clothes/bottom/pants2/pants2_red.png" },
+  { id: 8, name: "반바지_노랑", image: "/clothes/bottom/pants2/pants2_yellow.png" },
+  { id: 9, name: "반바지_흰색", image: "/clothes/bottom/pants2/pants2.png" },
+
+  // 긴바지
+  { id: 10, name: "긴바지_검정", image: "/clothes/bottom/pants/pants_black.png" },
+  { id: 11, name: "긴바지_하늘", image: "/clothes/bottom/pants/pants_blue.png" },
+  { id: 12, name: "긴바지_초록", image: "/clothes/bottom/pants/pants_green.png" },
+  { id: 13, name: "긴바지_인디고", image: "/clothes/bottom/pants/pants_indigo.png" },
+  { id: 14, name: "긴바지_갈색", image: "/clothes/bottom/pants/pants_orange.png" },
+  { id: 15, name: "긴바지_분홍", image: "/clothes/bottom/pants/pants_purple.png" },
+  { id: 16, name: "긴바지_빨강", image: "/clothes/bottom/pants/pants_red.png" },
+  { id: 17, name: "긴바지_노랑", image: "/clothes/bottom/pants/pants_yellow.png" },
+  { id: 18, name: "긴바지_흰색", image: "/clothes/bottom/pants/pants.png" },
+];
+
+
+// 외투 - 색상별
+export const overcoatOptions = [
+  // 코트
+  { id: 1, name: "코트_검정", image: "/clothes/overcoat/coat/coat_black.png" },
+  { id: 2, name: "코트_하늘", image: "/clothes/overcoat/coat/coat_blue.png" },
+  { id: 3, name: "코트_초록", image: "/clothes/overcoat/coat/coat_green.png" },
+  { id: 4, name: "코트_인디고", image: "/clothes/overcoat/coat/coat_indigo.png" },
+  { id: 5, name: "코트_갈색", image: "/clothes/overcoat/coat/coat_orange.png" },
+  { id: 6, name: "코트_분홍", image: "/clothes/overcoat/coat/coat_purple.png" },
+  { id: 7, name: "코트_빨강", image: "/clothes/overcoat/coat/coat_red.png" },
+  { id: 8, name: "코트_노랑", image: "/clothes/overcoat/coat/coat_yellow.png" },
+  { id: 8, name: "코트_흰색", image: "/clothes/overcoat/coat/coat.png" },
+
+  // 가디건
+  { id: 9, name: "가디건_검정", image: "/clothes/overcoat/cardigan/cardigan_black.png" },
+  { id: 10, name: "가디건_하늘", image: "/clothes/overcoat/cardigan/cardigan_blue.png" },
+  { id: 11, name: "가디건_초록", image: "/clothes/overcoat/cardigan/cardigan_green.png" },
+  { id: 12, name: "가디건_인디고", image: "/clothes/overcoat/cardigan/cardigan_indigo.png" },
+  { id: 13, name: "가디건_갈색", image: "/clothes/overcoat/cardigan/cardigan_orange.png" },
+  { id: 14, name: "가디건_분홍", image: "/clothes/overcoat/cardigan/cardigan_purple.png" },
+  { id: 15, name: "가디건_빨강", image: "/clothes/overcoat/cardigan/cardigan_red.png" },
+  { id: 16, name: "가디건_노랑", image: "/clothes/overcoat/cardigan/cardigan_yellow.png" },
+  { id: 16, name: "가디건_흰색", image: "/clothes/overcoat/cardigan/cardigan.png" },
+
+  // 숏패딩
+  { id: 17, name: "숏패딩_검정", image: "/clothes/overcoat/padding/padding_black.png" },
+  { id: 18, name: "숏패딩_하늘", image: "/clothes/overcoat/padding/padding_blue.png" },
+  { id: 19, name: "숏패딩_초록", image: "/clothes/overcoat/padding/padding_green.png" },
+  { id: 20, name: "숏패딩_인디고", image: "/clothes/overcoat/padding/padding_indigo.png" },
+  { id: 21, name: "숏패딩_갈색", image: "/clothes/overcoat/padding/padding_orange.png" },
+  { id: 22, name: "숏패딩_분홍", image: "/clothes/overcoat/padding/padding_purple.png" },
+  { id: 23, name: "숏패딩_빨강", image: "/clothes/overcoat/padding/padding_red.png" },
+  { id: 24, name: "숏패딩_노랑", image: "/clothes/overcoat/padding/padding_yellow.png" },
+  { id: 24, name: "숏패딩_흰색", image: "/clothes/overcoat/padding/padding.png" },
+];
+
+
+// 신발 - 색상별
+export const shoesOptions = [
+  { id: 1, name: "운동화_검정", image: "/clothes/shoes/shoes/shoes_black.png" },
+  { id: 2, name: "운동화_하늘", image: "/clothes/shoes/shoes/shoes_blue.png" },
+  { id: 3, name: "운동화_초록", image: "/clothes/shoes/shoes/shoes_green.png" },
+  { id: 4, name: "운동화_인디고", image: "/clothes/shoes/shoes/shoes_indigo.png" },
+  { id: 5, name: "운동화_갈색", image: "/clothes/shoes/shoes/shoes_orange.png" },
+  { id: 6, name: "운동화_분홍", image: "/clothes/shoes/shoes/shoes_purple.png" },
+  { id: 7, name: "운동화_빨강", image: "/clothes/shoes/shoes/shoes_red.png" },
+  { id: 8, name: "운동화_노랑", image: "/clothes/shoes/shoes/shoes_yellow.png" },
+  { id: 9, name: "운동화_흰색", image: "/clothes/shoes/shoes/shoes.png" },
+];
+
+
+// 악세서리 - 색상별
+export const accessoriesOptions = [
+  { id: 1, name: "마스크_검정", image: "/clothes/accessory/mask/mask_black.png" },
+  { id: 7, name: "마스크_빨강", image: "/clothes/accessory/mask/mask_red.png" },
+  { id: 9, name: "마스크_흰색", image: "/clothes/accessory/mask/mask.png" },
+];
+
+
+// 헤어스타일 - 앞머리 유무 - 색상별
+export const frontHairOptions = [
+  { id: 1, name: "앞머리 O_검정", image: "/clothes/hair/frontHair1/frontHair1_black.png" },
+  { id: 2, name: "앞머리 O_갈색", image: "/clothes/hair/frontHair1/frontHair1_brown.png" },
+  { id: 3, name: "앞머리 O_금발", image: "/clothes/hair/frontHair1/frontHair1_yellow.png" },
+  { id: 4, name: "앞머리 O_모양", image: "/clothes/hair/frontHair1/frontHair1.png" },
+
+  { id: 5, name: "앞머리 X_검정", image: "/clothes/hair/frontHair2/frontHair2_black.png" },
+  { id: 6, name: "앞머리 X_갈색", image: "/clothes/hair/frontHair2/frontHair2_brown.png" },
+  { id: 7, name: "앞머리 X_금발", image: "/clothes/hair/frontHair2/frontHair2_yellow.png" },
+  { id: 8, name: "앞머리 X_모양", image: "/clothes/hair/frontHair2/frontHair2.png" },
+];
+
+
+// 뒷머리 스타일 - 색상별
+export const backHairOptions = [
+  { id: 1, name: "숏컷_검정", image: "/clothes/hair/backHair1/backHair1_black.png" },
+  { id: 2, name: "숏컷_갈색", image: "/clothes/hair/backHair1/backHair1_brown.png" },
+  { id: 3, name: "숏컷_금발", image: "/clothes/hair/backHair1/backHair1_yellow.png" },
+  { id: 4, name: "숏컷_모양", image: "/clothes/hair/backHair1/backHair1.png" },
+
+  { id: 5, name: "장발_검정", image: "/clothes/hair/backHair2/backHair2_black.png" },
+  { id: 6, name: "장발_갈색", image: "/clothes/hair/backHair2/backHair2_brown.png" },
+  { id: 7, name: "장발_금발", image: "/clothes/hair/backHair2/backHair2_yellow.png" },
+  { id: 8, name: "장발_모양", image: "/clothes/hair/backHair2/backHair2.png" },
+
+  { id: 9, name: "단발_검정", image: "/clothes/hair/backHair3/backHair3_black.png" },
+  { id: 10, name: "단발_갈색", image: "/clothes/hair/backHair3/backHair3_brown.png" },
+  { id: 11, name: "단발_금발", image: "/clothes/hair/backHair3/backHair3_yellow.png" },
+  { id: 12, name: "단발_모양", image: "/clothes/hair/backHair3/backHair3.png" },
+];
